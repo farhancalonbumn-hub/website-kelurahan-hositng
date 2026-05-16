2af9e1325f72e98b98ecdc68da910932ecd0ddc9
farhancalonbumn-hub <farhancalonbumn@gmail.com>
2026-05-16 19:31:30 +0700

    Update SuratDomisiliController.php

--- app/Http/Controllers/SuratDomisiliController.php
+++ app/Http/Controllers/SuratDomisiliController.php
@@ -22,7 +22,6 @@ class SuratDomisiliController extends Controller
 public function store(Request $request)
 {
     
-    dd($request->all(), $_FILES);
     
        $validated = $request->validate([
 
