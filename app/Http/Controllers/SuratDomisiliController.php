41d15dd877d797908cd03550ce0627f04e507332
farhancalonbumn-hub <farhancalonbumn@gmail.com>
2026-05-16 19:59:05 +0700

    Update SuratDomisiliController.php

--- app/Http/Controllers/SuratDomisiliController.php
+++ app/Http/Controllers/SuratDomisiliController.php
@@ -21,7 +21,10 @@ class SuratDomisiliController extends Controller
 // ================= SIMPAN DOMISILI =================
 public function store(Request $request)
 {
-       
+    dd([
+    'upload_max_filesize' => ini_get('upload_max_filesize'),
+    'post_max_size' => ini_get('post_max_size'),
+]);   
     
        $validated = $request->validate([
 
